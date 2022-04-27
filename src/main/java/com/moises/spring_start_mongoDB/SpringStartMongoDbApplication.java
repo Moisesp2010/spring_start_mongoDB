c05a1f2fe2d7df7bad1ea97032b8377a653d503f
MOISES PEREIRA DO NASCIMENTO <83645201+Moisesp2010@users.noreply.github.com>
2022-04-27 07:50:01 -0300

    Primeiro comnite

---
+++ src/main/java/com/moises/spring_start_mongoDB/SpringStartMongoDbApplication.java
@@ -0,0 +1,15 @@
+package com.moises.spring_start_mongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStartMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStartMongoDbApplication.class, args);
+		
+		
+	}
+
+}
