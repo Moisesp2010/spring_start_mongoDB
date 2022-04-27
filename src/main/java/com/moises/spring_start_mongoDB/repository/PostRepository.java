9ef85bfcc6631a9fb5d62cd926bf9efa9f5ae448
MOISES PEREIRA DO NASCIMENTO <83645201+Moisesp2010@users.noreply.github.com>
2022-04-27 10:25:25 -0300

    Consulta simples com @query

--- src/main/java/com/moises/spring_start_mongoDB/repository/PostRepository.java
+++ src/main/java/com/moises/spring_start_mongoDB/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.moises.spring_start_mongoDB.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.moises.spring_start_mongoDB.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
